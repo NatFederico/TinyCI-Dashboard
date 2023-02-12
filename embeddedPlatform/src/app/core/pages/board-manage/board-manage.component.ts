7abe1da4d0ae6d7cfe08972e8fc5905b47d9e9f4
Federico Natali <federiconatali.dev@gmail.com>
2023-02-12 09:58:47 +0100

    Retrive table data and create form

--- embeddedPlatform/src/app/core/pages/board-manage/board-manage.component.ts
+++ embeddedPlatform/src/app/core/pages/board-manage/board-manage.component.ts
@@ -2,6 +2,8 @@ import { HttpClient } from "@angular/common/http";
 import { Component, OnInit } from "@angular/core";
 import { initSupabase } from "src/app/utils/initSupabase";
 import { MQTT, IClientOptions } from 'mqtt/dist/mqtt';
+import { ApiService } from '../../services/api.service';
+import { createClient, SupabaseClient } from '@supabase/supabase-js'
 
 @Component({
     selector: 'app-board-manage',
@@ -10,36 +12,49 @@ import { MQTT, IClientOptions } from 'mqtt/dist/mqtt';
 })
 export class BoardManageComponent implements OnInit {
     fields = [];
-    client;
+    boards: {
+        id: string;
+        name: string;
+        template: JSON;
+    }[]
+    supabase: SupabaseClient = createClient(initSupabase.supabaseUrl, initSupabase.supabaseKey);
+    // client;
 
-    constructor(private http: HttpClient) { }
 
-    ngOnInit(): void {
+    constructor(private http: HttpClient, private api: ApiService) { }
+
+    ngOnInit() {
         this.getFields();
-        this.connectToMQTT();
+        // this.connectToMQTT();
     }
 
-    getFields() {
-        const url = initSupabase.supabaseUrl;
-        this.http.get(url)
-            .subscribe(response => {
-                const keys = Object.keys(response[0]);
-                this.fields = keys.map(key => ({ name: key, type: 'text', value: response[0][key] }));
-            });
+
+
+    async getFields() {
+        const { data, error } = await this.supabase
+            .from('boards')
+            .select();
+        this.boards = data;
+        const jsonObject = this.boards[0];
+        for(const key in jsonObject){
+            if(jsonObject.hasOwnProperty(key)){
+                this.fields.push({name: key, label: key, value: jsonObject[key]});
+            }
+        }
     }
 
-    connectToMQTT() {
-        const options: IClientOptions = {
-            host: '{your_mqtt_broker_host}',
-            port: '{your_mqtt_broker_port}',
-            username: '{your_mqtt_broker_username}',
-            password: '{your_mqtt_broker_password}',
-            clientId: '{your_mqtt_client_id}'
-        };
+    // connectToMQTT() {
+    //     const options: IClientOptions = {
+    //         host: '{your_mqtt_broker_host}',
+    //         port: '{your_mqtt_broker_port}',
+    //         username: '{your_mqtt_broker_username}',
+    //         password: '{your_mqtt_broker_password}',
+    //         clientId: '{your_mqtt_client_id}'
+    //     };
 
-        this.client = new MQTT.Client(options);
-        this.client.connect();
-    }
+    //     this.client = new MQTT.Client(options);
+    //     this.client.connect();
+    // }
 
     submitForm() {
         const payload = JSON.stringify(this.fields.reduce((acc, field) => {
@@ -47,6 +62,6 @@ export class BoardManageComponent implements OnInit {
             return acc;
         }, {}));
 
-        this.client.publish('{your_mqtt_topic}', payload);
+        // this.client.publish('{your_mqtt_topic}', payload);
     }
 }
