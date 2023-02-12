edf14cb639c4fc9935d0c8bf602fd921caddac4f
Federico Natali <federiconatali.dev@gmail.com>
2023-02-12 07:37:34 +0100

    Initial board form

--- embeddedPlatform/src/app/core/pages/board-manage/board-manage.component.ts
+++ embeddedPlatform/src/app/core/pages/board-manage/board-manage.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from "@angular/common/http";
 import { Component, OnInit } from "@angular/core";
+import { initSupabase } from "src/app/utils/initSupabase";
 
 @Component({
     selector: 'app-board-manage',
@@ -6,10 +8,24 @@ import { Component, OnInit } from "@angular/core";
     styleUrls: ['./board-manage.component.scss']
 })
 export class BoardManageComponent implements OnInit {
-    
-    constructor() { }
+    fields = [];
+
+    constructor(private http: HttpClient) {}
 
     ngOnInit(): void {
-        
+        this.getFields();
     }
+
+    getFields() {
+        const url = initSupabase.supabaseUrl;
+        this.http.get(url)
+          .subscribe(response => {
+            const keys = Object.keys(response[0]);
+            this.fields = keys.map(key => ({ name: key, type: 'text', value: response[0][key] }));
+          });
+      }
+
+      submitForm() {
+        console.log(this.fields);
+      }
 }
